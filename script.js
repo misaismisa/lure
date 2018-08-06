8594680707c9beb1ed7596533332bef2c6f24b26
Misa Nguyen <misanguyen1390@gmail.com>
2018-08-06 15:02:02 -0700

    optimized images

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('.slideshow').slick({
-    dots: true,
+  dots: true,
   infinite: false,
   speed: 300,
   slidesToShow: 3,
@@ -19,14 +19,17 @@ $(document).ready(function(){
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
       }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        arrows: false
       }
     }
     // You can unslick at a given breakpoint now by adding:
